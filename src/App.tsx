8e0491cac3f19299005ceb3d7ef97eb7c4a8cef9
Daeun Yang <daeun.yang@elicer.com>
2022-11-16 00:23:59 +0900

    chore: set initial code with msw

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import GlobalStyle from 'styled-components'
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
 
-  // if (isServer) {
-  //   (async () => {
-  //     const { server } = await import('./mocks/server');
-  //     server.listen();
-  //   })();
-  // } else {
-  //   (async () => {
-  //     const { worker } = await import('./mocks/browser');
-  //     worker.start();
-  //   })();
-  // }
+function App() {   
+ 
+  const [fruits, setFruits] = React.useState<string[]>([])
+  
+  const getFruits = async () => {
+    try {
+      const response =  await (await fetch("/fruits")).json()
+      setFruits(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
-function App() {
+  React.useEffect(() => {
+   getFruits()
+  },[])
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>{fruits.join(', ')}</h1>
     </div>
   );
 }
