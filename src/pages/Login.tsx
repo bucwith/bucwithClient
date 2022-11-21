6234d84256ab694b3f43acb5c302fb2b7393003a
Daeun Yang <daeun.yang@elicer.com>
2022-11-22 00:52:39 +0900

    design: image wrapper 분리

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { ButtonColor } from "../@types/enums";
 import Button from "../components/Button";
 import Title from "../components/Title";
-import { VerticalCentered } from "../components/Wrapper";
+import { ImagedWrapper, VerticalCentered } from "../components/Wrapper";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,14 +11,16 @@ const Login = () => {
     return navigate("/nickname");
   };
   return (
-    <VerticalCentered gap="40px">
-      <Title primary={`오늘도 BucWith은\n당신의 바램을 응원해요.`} />
-      <Button
-        text="Google 계정으로 로그인"
-        color={ButtonColor.Google}
-        onClick={handleLoginClick}
-      />
-    </VerticalCentered>
+    <ImagedWrapper>
+      <VerticalCentered gap="40px">
+        <Title primary={`오늘도 BucWith은\n당신의 바램을 응원해요.`} />
+        <Button
+          text="Google 계정으로 로그인"
+          color={ButtonColor.Google}
+          onClick={handleLoginClick}
+        />
+      </VerticalCentered>
+    </ImagedWrapper>
   );
 };
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Title from "../components/Title";
 import InputBox from "../components/main/InputBox";
-import { VerticalCentered } from "../components/Wrapper";
+import { ImagedWrapper, VerticalCentered } from "../components/Wrapper";
 
 const Main = () => {
   return (
-    <VerticalCentered gap="40px">
-      <Title primary={`소현님이\n꿈꾸는 버킷리스트를\n적어주세요.`}></Title>
-      <InputBox
-        title="어떤 종류의 버킷리스트인가요?"
-        buttonText="응원 메시지 보러가기"
-        placeholder="예) 매일 운동하기 / 술 끊기"
-        textarea
-      />
-    </VerticalCentered>
+    <ImagedWrapper>
+      <VerticalCentered gap="40px">
+        <Title primary={`소현님이\n꿈꾸는 버킷리스트를\n적어주세요.`}></Title>
+        <InputBox
+          title="어떤 종류의 버킷리스트인가요?"
+          buttonText="응원 메시지 보러가기"
+          placeholder="예) 매일 운동하기 / 술 끊기"
+          textarea
+        />
+      </VerticalCentered>
+    </ImagedWrapper>
   );
 };
 
