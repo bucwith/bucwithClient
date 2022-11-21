9d684583a57ac6c5eed016822b9c7514a1e0995f
Daeun Yang <daeun.yang@elicer.com>
2022-11-22 00:53:20 +0900

    msw: 닉네임 설정 mock api 코드 구현

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,9 +1,16 @@
-import { rest } from 'msw'
+import { rest } from "msw";
 
-// [Get] fruits 서버 요청 시 [ '사과', '바나나' ]를 응답한다.
 export const handlers = [
-    rest.get( '/fruits', (req, res, ctx) => {
-        return res(ctx.status(200),ctx.json([ '사과', '바나나' ]))
-    })
-
-]
+  rest.put("/user/name", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: 200,
+        codeMsg: "SUCCESS",
+        data: {
+          name: "test",
+        },
+      })
+    );
+  }),
+];
