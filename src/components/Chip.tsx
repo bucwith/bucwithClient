b56813b915422e7f6cfc622caad03051dae50ea0
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-27 12:52:54 +0900

    Design: Chip 추가 및 적용

---
+++ src/components/Chip.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from "styled-components";
+
+interface Chiptypes {
+    text: string;
+    isFocus: boolean;
+}
+
+function Chip( { text, isFocus }: Chiptypes) {
+    return (
+        <div>
+            {isFocus ? 
+                <ChipFocusButton>
+                    { text }
+                </ChipFocusButton> :
+                <ChipButton>
+                    { text }
+                </ChipButton> }
+        </div>
+    );
+}
+
+const ChipButton = styled.button`
+    padding: 10px 16px;
+    border-radius: 30px;
+`
+
+const ChipFocusButton = styled.button`
+    padding: 10px 16px;
+    background-color = white;
+    color: black;
+    border-radius: 30px;
+`
+
+export default Chip;
